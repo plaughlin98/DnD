dcd6178fb78fdf3db447a5ea4acc55ad4d80a911
plaughlin98 <plaughlin98@gmail.com>
2020-11-09 17:05:52 -0600

    Added Display Function for Each Dice Roller

--- script.js
+++ script.js
@@ -13,59 +13,100 @@ function getRandomInt(max) {
 }
 
 var handlers = {
-    rollDice: function() {
-        
+    // Displays the Dice Inputs/Outputs
+    displayDice: function(dice) {
+        var allDiceDivs = document.querySelector(".all-dice").getElementsByTagName("div");
+        var allDiceDivsList = Array.prototype.slice.call(allDiceDivs);
+        for (let i = 0; i < allDiceDivsList.length; i++) {
+            if(allDiceDivsList[i].classList != "hidden") {
+                allDiceDivsList[i].classList.add('hidden');
+            } 
+        }
+        die = document.getElementById(dice)
+        die.classList.add('frontAndCenter');
+        die.classList.remove('hidden');
     },
-    chooseDie: function() {
-
+    rollDice: function(dice) {
+        return getRandomInt(dice) + 1;
     },
     numOfRoll: function() {
 
     },
-    display: function(diceDisplay, die) {
-        console.log(diceDisplay);
-        console.log(rollDice(die));
-        document.querySelector(diceDisplay).innerText = `Total Roll Sum: ${rollDice(die)}`;
-    },
-    toggleAdvantage: function() {
+    displayRoll: function(diceDisplay, die) {
+        let modifier = parseInt(this.addModifier());
+        let roll = parseInt(this.rollDice(die));
+        let rollSumDisplay = document.querySelector(diceDisplay);
 
+        if(this.addModifier() > 0) {
+            rollSumDisplay.innerText = `Modifier: +${modifier} Roll: ${roll} Total Sum: ${roll + modifier} `;
+        } else if(this.addModifier() < 0){
+            rollSumDisplay.innerText = `Modifier: ${modifier} Roll: ${roll} Total Sum: ${roll + modifier} `;
+        } else {
+            rollSumDisplay.innerText = `Total Roll: ${roll}`;
+        }
     },
+
+    // Get Modifier Value and Add to this.display function
     addModifier: function() {
-
+        return modifier = document.getElementById('modifier').value;
     },
 }
 
 function rollDice(dice) {
-    return getRandomInt(dice) + 1;
+    
 }
+
+document.querySelector(".diceSelection").addEventListener('click', function(e){
+    switch(e.target.id) {
+        case "d4DiceSelector":
+            handlers.displayDice("d4Display");
+            break;
+        case "d6DiceSelector":
+            handlers.displayDice("d6Display");
+            break;
+        case "d8DiceSelector":
+            handlers.displayDice("d8Display");
+            break;
+        case "d10DiceSelector":
+            handlers.displayDice("d10Display");
+            break;
+        case "d12DiceSelector":
+            handlers.displayDice("d12Display");
+            break;
+        case "d20DiceSelector":
+            handlers.displayDice("d20Display");
+            break;
+        case "d100DiceSelector":
+            handlers.displayDice("d100Display");
+            break;
+    }
+})
 
 document.querySelector(".all-dice").addEventListener('click', function(e) {
     switch(e.target.id) {
         case "d4Dice":
-           handlers.display(".d4DiceSum", dice.d4Dice);
+           handlers.displayRoll(".d4DiceSum", dice.d4Dice);
            break;
         case "d6Dice":
-            handlers.display(".d6DiceSum", dice.d6Dice);
+            handlers.displayRoll(".d6DiceSum", dice.d6Dice);
             break;
         case "d8Dice":
-            handlers.display(".d8DiceSum", dice.d8Dice);
+            handlers.displayRoll(".d8DiceSum", dice.d8Dice);
             break;
         case "d10Dice":
-            handlers.display(".d10DiceSum", dice.d10Dice);
+            handlers.displayRoll(".d10DiceSum", dice.d10Dice);
             break;
         case "d12Dice":
-            handlers.display(".d12DiceSum", dice.d12Dice);
+            handlers.displayRoll(".d12DiceSum", dice.d12Dice);
             break;
         case "d20Dice":
-            handlers.display(".d20DiceSum", dice.d20Dice);
+            handlers.displayRoll(".d20DiceSum", dice.d20Dice);
             break;
         case "d100Dice":
-            handlers.display(".d100DiceSum", dice.d100Dice);
+            handlers.displayRoll(".d100DiceSum", dice.d100Dice);
             break;
         
     }
 })
-function chooseDie() {
 
-}
 
